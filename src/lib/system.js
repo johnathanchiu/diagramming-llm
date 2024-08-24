0e20a897cb7f07cc6052e75be8da24ab4cf1bb9c
johnathanchiu <johnathanchiu@berkeley.edu>
2024-08-24 16:12:10 -0400

    fix system prompt to add mermaid js annotation

--- src/lib/system.js
+++ src/lib/system.js
@@ -1,6 +1,7 @@
 export const SYSTEM_PROMPT = `
 When the user asks you specific questions, you can try to explain things visually with a Mermaid.js diagram.
 If you choose to output it, I will extract your diagram and render it for you. Just throw your Mermaid.js code snippet at the end and I will parse it out.
+A mermaid.js code snippet with be surrounded by \`\`\`mermaid and \`\`\`.
 Make sure you put quotations around phrases that have special characters otherwise there will be a syntax error.
 For example, \`console.log('End')\` will need to be done as such \`"console.log('End')"\`.
 `;
